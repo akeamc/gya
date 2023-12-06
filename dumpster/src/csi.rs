5a8d330d77e3b7b6d2d399d9935607177ae3d5e9
akeamc <ake@amcoff.net>
2023-12-06 17:14:29 +0100

    structure

---
+++ dumpster/src/csi.rs
@@ -0,0 +1,187 @@
+//! CSI extractor for [Nexmon](https://github.com/seemoo-lab/nexmon_csi)-patched
+//! BCM4366c0 chips.
+//!
+//! Nexmon CSI is encoded in UDP packets, which in turn are defined
+//! as follows:
+//!
+//! ```c
+//! struct csi_udp_frame {
+//!     struct ethernet_ip_udp_header hdrs;
+//!     uint16 kk1; // magic bytes 0x1111
+//!     int8 rssi;
+//!     uint8 fc; //frame control
+//!     uint8 SrcMac[6];
+//!     uint16 seqCnt;
+//!     uint16 csiconf;
+//!     uint16 chanspec;
+//!     uint16 chip;
+//!     uint32 csi_values[];
+//! } __attribute__((packed));
+//! ```
+//!
+//! [GitHub source](https://github.com/seemoo-lab/nexmon_csi/blob/fdb25ef0e4e1402e968bb644d4914ad1a3d0a84d/src/csi_extractor.c#L135-L146)
+
+use macaddr::MacAddr6;
+use num_complex::Complex;
+
+/// A reported CSI frame.
+#[derive(Debug, Clone)]
+pub struct Frame {
+    pub rssi: i8,
+    pub frame_control: u8,
+    pub source_mac: MacAddr6,
+    /// "The two byte sequence number of the Wi-Fi frame that triggered
+    /// the collection of the CSI contained in this packet."
+    pub seq_cnt: u16,
+    pub config: u16,
+    // core: u8,
+    // spatial: u8,
+    pub chan_spec: u16,
+    pub chip: u16,
+    pub csi_values: Vec<Complex<f32>>,
+}
+
+/// Channel bandwidth.
+#[derive(Debug, Clone, Copy)]
+pub enum Bandwidth {
+    /// 20 MHz
+    Bw20,
+    /// 40 MHz
+    Bw40,
+    /// 80 MHz
+    Bw80,
+    /// 160 MHz
+    Bw160,
+}
+
+impl Bandwidth {
+    const fn from_chan_spec(chan_spec: u16) -> Self {
+        match chan_spec & 0b11 {
+            0b00 => Self::Bw20,
+            0b01 => Self::Bw40,
+            0b10 => Self::Bw80,
+            0b11 => Self::Bw160,
+            _ => unreachable!(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Error {
+    NotEnoughBytes,
+    NotANexmonPacket,
+    MissingMagicBytes,
+}
+
+impl Frame {
+    pub const fn bandwidth(&self) -> Bandwidth {
+        Bandwidth::from_chan_spec(self.chan_spec)
+    }
+
+    pub fn from_slice(b: &[u8]) -> Result<Self, Error> {
+        if b.len() < 60 {
+            return Err(Error::NotEnoughBytes);
+        }
+
+        if &b[6..12] != b"NEXMON" {
+            return Err(Error::NotANexmonPacket);
+        }
+
+        if b[42..44] != [0x11, 0x11] {
+            return Err(Error::MissingMagicBytes);
+        }
+
+        let config = u16::from_le_bytes([b[54], b[55]]);
+        // let frame_no = u16::from_le_bytes([b[11], b[12]]);
+        // let core = b[11] & 0b111;
+        // let spatial = (b[11] >> 3) & 0b111;
+        // let spatial = ((b[10] | b[11]) >> 3) & 0x7;
+
+        let chan_spec = u16::from_le_bytes([b[56], b[57]]);
+        let csi_values = unpack_csi(Bandwidth::from_chan_spec(chan_spec), &b[60..]);
+
+        Ok(Self {
+            rssi: b[44] as i8,
+            frame_control: b[45],
+            source_mac: MacAddr6::new(b[46], b[47], b[48], b[49], b[50], b[51]),
+            seq_cnt: u16::from_le_bytes([b[52], b[53]]),
+            config,
+            // core,
+            // spatial,
+            chan_spec,
+            chip: u16::from_le_bytes([b[58], b[59]]),
+            csi_values,
+        })
+    }
+}
+
+/// CSI values are encoded in 32 bits each:
+///
+/// ```text
+/// sign(1) | re(12) | sign(1) | im(12) | exp(6)
+/// ```
+pub fn unpack_complex(i: u32) -> Complex<f32> {
+    const MAN_MASK: u32 = 0b111111111111; // 12 bits
+    const E_MASK: u32 = 0b111111; // 6 bits
+
+    let mut exp = (i & E_MASK) as i32;
+    if exp >= 1 << 5 {
+        // exponent is negative
+        exp -= 1 << 6;
+    }
+
+    let mut re = ((i >> 18) & MAN_MASK) as i32;
+    if i & 1 << 29 != 0 {
+        // sign bit for real part is set
+        re = -re;
+    }
+
+    let mut im = ((i >> 6) & MAN_MASK) as i32;
+    if i & 1 << 17 != 0 {
+        // sign bit for imaginary part is set
+        im = -im;
+    }
+
+    // construct float from mantissas and exponent
+    let re = (re as f32) * (2f32.powi(exp));
+    let im = (im as f32) * (2f32.powi(exp));
+
+    Complex::new(re, im)
+}
+
+/// Unpacks the CSI values from the given buffer.
+///
+/// The complex values are expected to be encoded in 32 bits each, with
+/// 11 bits for each mantissa and 6 bits for the exponent.
+///
+/// # Panics
+///
+/// Panics if the buffer is too short (`< 3.2 * <bandwidth in MHz>`).
+fn unpack_csi(bw: Bandwidth, b: &[u8]) -> Vec<Complex<f32>> {
+    // nfft = bw * 3.2
+    let nfft = match bw {
+        Bandwidth::Bw20 => 64,
+        Bandwidth::Bw40 => 128,
+        Bandwidth::Bw80 => 256,
+        Bandwidth::Bw160 => 512,
+    };
+
+    assert!(b.len() >= nfft * 4, "not enough data");
+
+    // unpack_float_acphy(
+    //   nbits: 10,
+    //   autoscale: 0,
+    //   shft: 0,
+    //   fmt: 1,
+    //   nman: 12,
+    //   nexp: 6,
+    //   *nfftp,
+    //   H,
+    //   Hout,
+    // );
+
+    b.chunks_exact(4)
+        .map(|b| u32::from_le_bytes(b.try_into().unwrap()))
+        .map(unpack_complex)
+        .collect()
+}
--- dumpster/src/main.rs
+++ dumpster/src/main.rs
@@ -1,25 +1,166 @@
-use pcap::PacketCodec;
+use std::f32::consts::PI;
 
-fn main() {
+use image::{ImageBuffer, Rgb, RgbImage};
+use num_complex::Complex;
+use palette::{rgb::Rgba, Hsl, IntoColor};
+
+pub mod csi;
+
+// fn unpack_float(buf: &[u8], csi: &mut Vec<Complex<f32>>, nfft: usize, M: usize, E: usize, endian: char) {
+//     let nbits = 10;
+//     let autoscale = 1;
+//     let e_p = 1 << (E - 1);
+//     let e_shift = 1;
+//     let e_zero = -(M as i32);
+//     let mut maxbit = -e_p;
+//     let k_tof_unpack_sgn_mask = 1 << 31;
+//     let ri_mask = (1 << (M - 1)) - 1;
+//     let e_mask = (1 << E) - 1;
+//     let sgnr_mask = 1 << (E + 2 * M - 1);
+//     let sgni_mask = sgnr_mask >> M;
+//     let mut he = vec![0; 256];
+//     let mut hout = vec![0; 512];
+
+//     for i in 0..nfft {
+//         let h = u32::from_le_bytes(buf[4 * i..4 * i + 4].try_into().unwrap());
+
+//         let v_real = (h >> (E + M)) & ri_mask;
+//         let v_imag = (h >> E) & ri_mask;
+//         let mut e = h & e_mask;
+//         if e >= e_p {
+//             e -= e_p << 1;
+//         }
+//         he[i] = e;
+//         let x = v_real | v_imag;
+
+//         if autoscale != 0 && x != 0 {
+//             let mut m = 0xffff0000;
+//             let mut b = 0xffff;
+//             let mut s = 16;
+//             while s > 0 {
+//                 if x & m != 0 {
+//                     e += s;
+//                     x >>= s;
+//                 }
+//                 s >>= 1;
+//                 m = (m >> s) & b;
+//                 b >>= s;
+//             }
+//             if e > maxbit {
+//                 maxbit = e;
+//             }
+//         }
+//         if h & sgnr_mask != 0 {
+//             hout[i << 1] |= k_tof_unpack_sgn_mask;
+//         }
+//         if h & sgni_mask != 0 {
+//             hout[(i << 1) + 1] |= k_tof_unpack_sgn_mask;
+//         }
+//         hout[i << 1] |= v_real;
+//         hout[(i << 1) + 1] |= v_imag;
+//     }
+
+//     let shft = nbits - maxbit;
+//     for i in 0..nfft * 2 {
+//         let e = he[i >> e_shift] + shft;
+//         let mut sgn: i32 = 1;
+//         if hout[i] & k_tof_unpack_sgn_mask != 0 {
+//             sgn = -1;
+//             hout[i] &= !k_tof_unpack_sgn_mask;
+//         }
+//         if e < e_zero {
+//             hout[i] = 0;
+//         } else if e < 0 {
+//             let e = -e;
+//             hout[i] = hout[i] >> e;
+//         } else {
+//             hout[i] = hout[i] << e;
+//         }
+//         hout[i] *= sgn;
+//     }
+
+//     for i in 0..nfft {
+//         csi[i] = Complex::new(hout[i * 2] as f32, hout[i * 2 + 1] as f32);
+//     }
+// }
+
+/// Plot a complex vector as an image.
+fn plot_complex(nums: &[Complex<f32>], width: u32, height: u32) -> RgbImage {
+    let mut image = RgbImage::new(width, height);
+
+    let max_norm = nums
+        .iter()
+        .map(|c| c.norm())
+        .max_by(|a, b| a.partial_cmp(b).unwrap())
+        .unwrap();
+
+    for (i, c) in nums.iter().enumerate() {
+        let x = i as u32 % width;
+        let y = i as u32 / width;
+
+        let l = (1. - c.norm() / max_norm) / 2.;
+        // let l = 0.5 * c.norm() / max_norm;
+        // let l = 0.5;
+
+        let color: Rgba = Hsl::new((c.arg() + PI).to_degrees(), 1.0, l).into_color();
+        // let color: Rgba = Rgba::new(l, l, l, 1.0);
+        let pixel = Rgb([
+            (color.red * 255.0) as u8,
+            (color.green * 255.0) as u8,
+            (color.blue * 255.0) as u8,
+        ]);
+
+        image.put_pixel(x, y, pixel);
+    }
+
+    image
+}
+
+fn main() -> anyhow::Result<()> {
     let mut capture = pcap::Capture::from_file("trace.pcap").unwrap();
 
-    dbg!(capture.get_datalink());
-
     let mut cnt = 0;
+    let mut csi = Vec::new();
 
-    loop {
+    while cnt < 4000 {
         let packet = match capture.next_packet() {
             Ok(packet) => packet,
             Err(pcap::Error::NoMorePackets) => break,
             Err(err) => panic!("error while reading packet: {}", err),
         };
 
-        dbg!(packet.header);
+        let frame = csi::Frame::from_slice(&packet.data).unwrap();
 
-        todo!();
+        // bcm4366c0 returns floating point CSI
+
+        // dbg!(csi.len());
+
+        csi.extend_from_slice(&frame.csi_values);
+
+        // for (i, c) in csi.iter().enumerate() {
+        //     let x = cnt;
+        //     let y = i as u32;
+
+        //     println!("{y}");
+
+        //     let color: Rgba = Hsl::new(c.arg() / TAU, 1.0, 0.5).into_color();
+        //     let pixel = Rgb([
+        //         (color.red * 255.0) as u8,
+        //         (color.green * 255.0) as u8,
+        //         (color.blue * 255.0) as u8,
+        //     ]);
+
+        //     image.put_pixel(x, y, pixel);
+        // }
 
         cnt += 1;
     }
 
+    let image = plot_complex(&csi, cnt, csi.len() as u32 / cnt);
+
     println!("{} packets", cnt);
+
+    image.save("csi.png")?;
+
+    Ok(())
 }
