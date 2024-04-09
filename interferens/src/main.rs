f315dcd9e3ee2f1fa33f5bca340d272ed0ef9f5a
akeamc <ake@amcoff.net>
2024-04-09 19:02:36 +0200

    Add interferens

--- interferens/src/main.rs
+++ interferens/src/main.rs
@@ -1,27 +1,62 @@
-use std::f64::consts::PI;
+use std::{f64::consts::PI, path::PathBuf};
 
+use clap::Parser;
+use colorgrad::CustomGradient;
 use nalgebra::Vector2;
 use plotters::prelude::*;
 
-const N_SOURCES: usize = 10;
-const SOURCE_SPACING: f64 = 1.0;
-const WAVELENGTH: f64 = 1.0;
-const ANGLE: f64 = 0.0;
+struct UniformLinearArray {
+    n_sources: usize,
+    source_spacing: f64,
+    wavelength: f64,
+    angle: f64,
+}
 
-fn amplitude(p: Vector2<f64>) -> f64 {
-    (0..N_SOURCES)
-        .map(|i| {
-            let i = i as f64;
-            let s = Vector2::new((i - N_SOURCES as f64 * 0.5) * SOURCE_SPACING, 0.0);
-            (((p - s).norm() * PI + ANGLE * i) * WAVELENGTH).sin()
-        })
-        .sum::<f64>() / N_SOURCES as f64
+impl UniformLinearArray {
+    fn amplitude(&self, p: Vector2<f64>) -> f64 {
+        (0..self.n_sources)
+            .map(|i| {
+                let i = i as f64;
+                let s = Vector2::new((i - (self.n_sources - 1) as f64 * 0.5) * self.source_spacing, 0.0);
+                (((p - s).norm() * PI + self.angle * i) * self.wavelength).sin()
+            })
+            .sum::<f64>()
+    }
+}
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[clap(short, default_value = "1")]
+    n: usize,
+    #[clap(short, long, default_value = "0.5")]
+    spacing: f64,
+    #[clap(short, long, default_value = "1.0")]
+    wavelength: f64,
+    #[clap(short, long, default_value = "0.0")]
+    angle: f64,
+    #[clap(short, long)]
+    out: PathBuf,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let root = BitMapBackend::new("interferens.png", (800, 800)).into_drawing_area();
+    let Args {
+        n,
+        spacing,
+        wavelength,
+        angle,
+        out,
+    } = Args::parse();
+
+    let root = BitMapBackend::new(&out, (800, 800)).into_drawing_area();
 
     root.fill(&WHITE)?;
+
+    let ula = UniformLinearArray {
+        n_sources: n,
+        source_spacing: spacing,
+        wavelength,
+        angle: angle.to_radians(),
+    };
 
     let mut chart = ChartBuilder::on(&root)
         .margin(20)
@@ -46,15 +81,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         (xr.end - xr.start) / pw as f64,
         (yr.end - yr.start) / ph as f64,
     );
-    for k in 0..(pw * ph) {
-        let (x, y) = (
-            xr.start + step.0 * (k % pw) as f64,
-            yr.start + step.1 * (k / pw) as f64,
-        );
+    let pixels = (0..(pw * ph))
+        .map(|k| {
+            let (x, y) = (
+                xr.start + step.0 * (k % pw) as f64,
+                yr.start + step.1 * (k / pw) as f64,
+            );
 
+            ((x, y), ula.amplitude(Vector2::new(x, y)))
+        })
+        .collect::<Vec<_>>();
+    let max = pixels
+        .iter()
+        .map(|(_, a)| a.abs())
+        .max_by(|a, b| a.total_cmp(b))
+        .unwrap();
+    let grad = CustomGradient::new()
+        .domain(&[-max, 0.0, max])
+        .html_colors(&["blue", "white", "red"])
+        .build()
+        .unwrap();
+    for ((x, y), v) in pixels {
+        let color = grad.at(v);
         plotting_area.draw_pixel(
             (x, y),
-            &BlackWhite::get_color(amplitude(Vector2::new(x, y))),
+            &RGBColor(
+                (color.r * 255.) as _,
+                (color.g * 255.) as _,
+                (color.b * 255.) as _,
+            ),
         )?;
     }
 
